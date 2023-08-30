cdae0b773cd87bc009742be9a6455c7843332989
Lukács Péter <petinyo007@gmail.com>
2023-08-30 21:00:34 +0200

    Verzió 12

--- script.js
+++ script.js
@@ -13,3 +13,29 @@ function toggleAccordion() {
 }
 
 items.forEach((item) => item.addEventListener("click", toggleAccordion));
+
+// Sticky navigation
+
+const sectionHeroEl = document.querySelector(".hero-section");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "80px",
+  }
+);
+obs.observe(sectionHeroEl);
